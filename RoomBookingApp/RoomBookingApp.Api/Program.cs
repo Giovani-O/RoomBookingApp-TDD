1c966482b0ff2147c80b04524de14aaaef021d0f
Giovani de Oliveira <giovani98@proton.me>
2024-05-09 15:49:53 -0300

    config: Setting up Sqlite

--- RoomBookingApp/RoomBookingApp.Api/Program.cs
+++ RoomBookingApp/RoomBookingApp.Api/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using RoomBookingApp.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = "DataSouce=:memory:";
+var connection = new SqliteConnection(connectionString);
+connection.Open();
+
+builder.Services.AddDbContext<RoomBookingAppDbContext>(
+    options => options.UseSqlite(connection)
+);
 
 var app = builder.Build();
 
